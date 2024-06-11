01d3162bc60322fd3ff3c8d596bf1e6b8c7ba47a
Hans Bouwman <hans.bouwman@protonmail.com>
2024-06-12 00:18:57 +0200

    fixed?

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+mkdir output
 mkdir output/figures
 
 # Set up the venv
 python3 -m venv venv
 source venv/bin/activate
+pip install --upgrade pip
 pip install --verbose -r requirements.txt
