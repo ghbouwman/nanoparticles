c41cefe52d803a48c6a2570a32172577247c4072
Hans Bouwman <hans.bouwman@protonmail.com>
2024-06-13 16:20:51 +0200

    Revert "removed some multithreading stuff"
    This reverts commit 924c3aefb66eb492797c31f27c1fbf07b8adcfea.

--- src/main.py
+++ src/main.py
@@ -7,7 +7,8 @@ from settings import *
 
 def main():
 
-    Process(target=simulate.simulate, args=(f"run1",)).start()
+    for i in range(1, NR_RUNS+1):
+        Process(target=simulate.simulate, args=(f"run_{i:02}",)).start()
 
 # Make sure code only runs when the file is executed as a script.
 if __name__ == "__main__":
