5991d6248254e3448f29d7ac717101530768c619
Hans Bouwman <hans.bouwman@protonmail.com>
2024-06-13 16:03:35 +0200

    removed more

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from settings import *
 
 def main():
 
-    Process(target=simulate.simulate, args=(f"run1",)).start()
+    simulate.simulate("run1")
 
 # Make sure code only runs when the file is executed as a script.
 if __name__ == "__main__":
