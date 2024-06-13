924c3aefb66eb492797c31f27c1fbf07b8adcfea
Hans Bouwman <hans.bouwman@protonmail.com>
2024-06-13 16:00:53 +0200

    removed some multithreading stuff

--- src/main.py
+++ src/main.py
@@ -7,8 +7,7 @@ from settings import *
 
 def main():
 
-    for i in range(1, NR_RUNS+1):
-        Process(target=simulate.simulate, args=(f"run_{i:02}",)).start()
+    Process(target=simulate.simulate, args=(f"run1",)).start()
 
 # Make sure code only runs when the file is executed as a script.
 if __name__ == "__main__":
