9984437c1a8c8050fabb2d737367aea8a5f226d7
Hans Bouwman <hans.bouwman@protonmail.com>
2024-06-13 16:22:11 +0200

    readded multithreaing

--- src/main.py
+++ src/main.py
@@ -7,8 +7,10 @@ from settings import *
 
 def main():
 
-    for i in range(1, NR_RUNS+1):
-        Process(target=simulate.simulate, args=(f"run_{i:02}",)).start()
+    for i in range(NR_RUNS):
+        Process(target=simulate.simulate, args=(f"run{i}",)).start()
+
+    print("Started all runs.")
 
 # Make sure code only runs when the file is executed as a script.
 if __name__ == "__main__":
--- src/settings.py
+++ src/settings.py
@@ -10,13 +10,13 @@ MAX_RESISTANCE = 1e15 # max resistance allowed in the distance computation
 MAX_PARTICLES = 20_000 # high numbers wil cause large memory to be used
 
 # np.log(np.finfo(np.float32))
-NR_RUNS = 1
+NR_RUNS = 10
 NR_STEPS = 1000 # Number of iterations in the simulation loop
 DELTA_T = 1e-12 # Simulation timestep
 
 # Plotting
-IMAGE_SIZE = 500
-PLOTTING = True
+IMAGE_SIZE = 1000
+PLOTTING = False
 
 # Physical constants:
 ELEMENTARY_CHARGE = 1.6e-19
@@ -31,11 +31,7 @@ SCHRODINGER_CONSTANT = 2*ELECTRON_MASS / HBAR**2
 # Material constants for molybdenum:
 MO_MASS = 95.95 * DALTON
 MO_CHARGE = 1 * ELEMENTARY_CHARGE
-# RESISTIVITY = 53.4e-9 # Temperature dependent?
-# MATERIAL_CHARGE_DENSITY = 1e-3
-# TOUCHING_RESISTANCE = 1e-7
 BREAKAWAY_ENERGY = 6.4e-16
-# BREAKAWAY_ENERGY *= 30 # this is a magic value, needs investigation
 WORK_FUNCTION_MO = 7.44e-19 # 6.985~7.931; GM: 7.44, 4.65 eV
 TUNNELING_SCALE = 1 / sqrt(SCHRODINGER_CONSTANT*WORK_FUNCTION_MO)
 MATERIAL_CHARGE_DENSITY = MO_CHARGE/MO_MASS # 1e8; not sure what to do with this; might need to be an order of 36 higher at most; this would increase the speed at which the filaments form
