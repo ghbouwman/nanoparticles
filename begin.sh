ccfc6727aabdb6e173f730c2566fd3fba498f214
Hans Bouwman <hans.bouwman@protonmail.com>
2024-06-14 16:12:03 +0200

    reduced sleep time

--- begin.sh
+++ begin.sh
@@ -4,7 +4,7 @@ for index in {0..9}; do
     run_name="run$index"
     echo $run_name > output/index.txt
     sbatch -N1 --tasks-per-node=1 --hint=multithread -c3 --mem-per-cpu=8G --job-name=$run_name --time="2-2" --output="output/$run_name.out" Runfile.sh
-    sleep 5
+    sleep 2
 done;
 
 echo $(squeue -u $USER)
