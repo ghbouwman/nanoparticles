bd79d353293aa3accfb851063d43f3f4a92e3ab3
Hans Bouwman <hans.bouwman@protonmail.com>
2024-06-14 15:05:01 +0200

    typo

--- begin.sh
+++ begin.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-for index in {0..9}
+for index in {0..9}; do
     run_name="run$index"
     echo "RUN_NAME = $run_name" > src/index.py
-    sbatch -N1 --tasks-per-node=1 --hint=multithread -c3 --mem-per-cpu=8G --job-name="$run_name" -t=2-2 Runfile.sh
+    sbatch -N1 --tasks-per-node=1 --hint=multithread -c3 --mem-per-cpu=8G --job-name=$run_name -t=2-2 Runfile.sh
+done;
