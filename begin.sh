1bb56d749c414733b1899a017b5028cb7796506e
Hans Bouwman <hans.bouwman@protonmail.com>
2024-06-14 16:11:02 +0200

    added squeue

--- begin.sh
+++ begin.sh
@@ -6,3 +6,5 @@ for index in {0..9}; do
     sbatch -N1 --tasks-per-node=1 --hint=multithread -c3 --mem-per-cpu=8G --job-name=$run_name --time="2-2" --output="output/$run_name.out" Runfile.sh
     sleep 5
 done;
+
+echo $(squeue -u $USER)
