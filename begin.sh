ec50bea4e303fd7e1893236402731f2042a3d603
Hans Bouwman <hans.bouwman@protonmail.com>
2024-06-14 16:43:18 +0200

    increased time in sleep

--- begin.sh
+++ begin.sh
@@ -4,7 +4,7 @@ for index in {0..9}; do
     run_name="run$index"
     echo $run_name\n > output/index.txt
     sbatch -N1 --tasks-per-node=1 --hint=multithread -c3 --mem-per-cpu=8G --job-name=$run_name --time="2-2" --output="output/$run_name.out" Runfile.sh
-    sleep 2
+    sleep 10
     squeue -u $USER
 done;
 
